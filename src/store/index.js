60b16dce17faa596e19b3bd9b58c2dd0a06c2c69
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2021-07-22 10:11:38 +0900

    fetch evetns

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    events: null
   },
   mutations: {
+    setEvents(state, payload) {
+      state.events = payload
+    }
   },
   actions: {
+    async fetchEvents(store) {
+      if (store.state.events !== null) return;
+
+      const response = await fetch(
+        'https://admob-app-id-5452967350.firebaseio.com/events.json'
+      ).then((res) => {
+        return res.json();
+      });
+      store.commit("setEvents", response);
+    }
   },
   modules: {
   }
