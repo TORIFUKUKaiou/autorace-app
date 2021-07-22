01a2f2dcb100b5d88581e1dc8919531ed15ab8db
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2021-07-22 22:00:49 +0900

    Updates

---
+++ precache-manifest.6c5be9efb748c0d269868b46db3733ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd2b062dfd95a1fa0a79",
+    "url": "autorace-app/css/chunk-vendors.72756fa2.css"
+  },
+  {
+    "revision": "18b0da96c9ee7c844787ada3fd600038",
+    "url": "autorace-app/index.html"
+  },
+  {
+    "revision": "855b22fa8a1ab985925b",
+    "url": "autorace-app/js/about.b6e996e5.js"
+  },
+  {
+    "revision": "9840f6c0a80c429fd1a9",
+    "url": "autorace-app/js/app.15ed1107.js"
+  },
+  {
+    "revision": "dd2b062dfd95a1fa0a79",
+    "url": "autorace-app/js/chunk-vendors.de0f2d18.js"
+  },
+  {
+    "revision": "bd70ce27f93e77ef9251fc621af48c4b",
+    "url": "autorace-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "autorace-app/robots.txt"
+  }
+]);
